da0c5394f40804466fef77f003b52372371c24b2
emermacko <mr.macko.yt@gmail.com>
2019-10-07 21:48:03 +0200

    Update Horner.java

--- funkcje/Horner.java
+++ funkcje/Horner.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
+public static double horner(int stopienWielomianu, double[] wspolczynniki, double argument) {
+	double wynik = wspolczynniki[stopienWielomianu];
 
-public class Horner {
-	public static Scanner read = new Scanner(System.in);
-		
-	public static double horner(int stopienWielomianu, double[] wspolczynniki, double argument) {
-		double wynik = wspolczynniki[stopienWielomianu];
-		
-		for(int i=stopienWielomianu-1; i>=0; i--) {
-			wynik = wynik * argument + wspolczynniki[i];
-		}
-		return wynik;
+	for(int i=stopienWielomianu-1; i>=0; i--) {
+		wynik = wynik * argument + wspolczynniki[i];
 	}
+	return wynik;
+}
