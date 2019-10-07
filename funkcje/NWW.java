c3cce91e95b451fc59a0818e583af3c302ad4ad5
emermacko <mr.macko.yt@gmail.com>
2019-10-07 21:47:15 +0200

    Create NWW.java

---
+++ funkcje/NWW.java
@@ -0,0 +1,12 @@
+public static int NWD (int a, int b) {
+  while(b!=0) {
+    int Pomoc = a%b;
+    a=b;
+    b=Pomoc;
+  }
+  return a;
+}
+  
+public static int NWW (int a, int b) {
+  return (a*b)/NWD(a,b);
+}
