c612d55b6112b1b8d348736c7330ebeca6e6858e
emermacko <mr.macko.yt@gmail.com>
2019-10-07 22:16:18 +0200

    Create PrzezWybor.java

---
+++ funkcje/sortowania/PrzezWybor.java
@@ -0,0 +1,17 @@
+public static void sortowanie(int tab[]) {
+	int pomoc;
+	int min;
+	for(int k=0; k<tab.length-1; k++) {
+		min = k;
+		for(int i=k+1; i<tab.length; i++) {
+				if(tab[i] < tab[min]) {
+					min = i;
+				}
+		}
+		pomoc = tab[k];
+		tab[k] = tab[min];
+		tab[min] = pomoc;
+	}
+	System.out.println();System.out.println();
+	wypisywanie(tab);
+}
