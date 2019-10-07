0eb8aa50ab54df5540f54759a02135495185c07c
emermacko <mr.macko.yt@gmail.com>
2019-10-07 22:09:24 +0200

    Create babelkowe.java

---
+++ funkcje/sortowania/babelkowe.java
@@ -0,0 +1,14 @@
+public static void sortowanie(int tab[]) {
+	int pomoc;
+	for(int j=tab.length-1; j>0; j--) {
+		for(int i=0; i<j; i++) {
+				if(tab[i] > tab[i+1]) {
+					pomoc = tab[i];
+					tab[i] = tab[i+1];
+					tab[i+1] = pomoc;
+				}
+		}
+	}
+	System.out.println("\n");
+	wypisywanie(tab);
+}
