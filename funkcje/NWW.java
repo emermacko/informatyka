d653ffb9cec3f13f313d57021faaa829a7192032
emermacko <mr.macko.yt@gmail.com>
2019-10-07 22:06:35 +0200

    Update NWW.java

--- funkcje/NWW.java
+++ funkcje/NWW.java
@@ -1,12 +1,12 @@
 public static int NWD (int a, int b) {
-  while(b!=0) {
-    int Pomoc = a%b;
-    a=b;
-    b=Pomoc;
-  }
-  return a;
+	while(b!=0) {
+		int Pomoc = a%b;
+		a=b;
+		b=Pomoc;
+	}
+	return a;
 }
   
 public static int NWW (int a, int b) {
-  return (a*b)/NWD(a,b);
+	return (a*b)/NWD(a,b);
 }
