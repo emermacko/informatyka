b6334efa33eca676c4515d3b01227e66ec65dec3
emermacko <mr.macko.yt@gmail.com>
2019-10-07 22:12:06 +0200

    Update Horner.java

--- funkcje/Horner.java
+++ funkcje/Horner.java
@@ -1,8 +1,1 @@
-public static double horner(int stopienWielomianu, double[] wspolczynniki, double argument) {
-	double wynik = wspolczynniki[stopienWielomianu];
 
-	for(int i=stopienWielomianu-1; i>=0; i--) {
-		wynik = wynik*argument + wspolczynniki[i];
-	}
-	return wynik;
-}
