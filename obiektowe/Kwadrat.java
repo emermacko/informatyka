539f0f3379d35745bdfb617186e91a44d57a3b7e
emermacko <mr.macko.yt@gmail.com>
2019-10-08 13:56:01 +0200

    Update Kwadrat.java

--- obiektowe/Kwadrat.java
+++ obiektowe/Kwadrat.java
@@ -2,14 +2,27 @@ package Obiektowe;
 
 public class Kwadrat {
 	
+	static int ID=1;
+	
 	private double bok;
+	private int id = 1;
 	
 	Kwadrat(){
-		bok = 1;
+		this(1);
 	}
 	
 	public Kwadrat(double bok) {
-		this.bok = bok;
+		this(bok, "cos innego");
+	}
+	
+	public Kwadrat(double bok, String s) {
+		this.id = ID; ID++;
+		if(s.equals("km")) this.bok = bok*1000;
+		else this.bok = bok;
+	}
+	
+	public int getID() {
+		return this.id;
 	}
 	
 	public void setBok(double bok) {
@@ -26,6 +39,10 @@ public class Kwadrat {
 	
 	public double obliczObwod() {
 		return this.bok*4;
+	}
+	
+	public String toString() {
+		return "ID:" + this.getID() + " Bok: " + this.getBok() + " Pole: " + this.obliczPole();
 	}
 
 }
