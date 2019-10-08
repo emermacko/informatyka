08e090664f2b4d3b14e7f643e8dc2da7aee1b0e3
emermacko <mr.macko.yt@gmail.com>
2019-10-08 13:55:53 +0200

    Update Main.java

--- obiektowe/Main.java
+++ obiektowe/Main.java
@@ -4,33 +4,42 @@ import java.util.Scanner;
 
 public class Main {
 	
+	public static Scanner read = new Scanner(System.in);
+	
 	public static void Kwadrat() {
-		Scanner read = new Scanner(System.in);
 		double bok;
 		
 		do {
 			System.out.print("Podaj bok kwadartu [ > ] ");
 			bok = read.nextDouble();
-		} while(bok<=0); read.close();
+		} while(bok<=0);
 		
 		Kwadrat kw1 = new Kwadrat(bok);
-		//Kwadrat kw1 = new Kwadrat();
 		
-		System.out.print("\nObwod kwadratu o boku [" + kw1.getBok() + "] = ");
+		System.out.print("Obwod kwadratu o boku [" + kw1.getBok() + "] = ");
 		System.out.println(kw1.obliczObwod());
 		System.out.print("Pole kwadratu o boku [" + kw1.getBok() + "] = ");
 		System.out.println(kw1.obliczPole());
 
 	}
 	
+	public static void Kwadrat2() {
+		Kwadrat kw[] = new Kwadrat[3];
+		kw[0] = new Kwadrat(2.6);
+		kw[1] = new Kwadrat();
+		kw[2] = new Kwadrat(3, "km");
+		
+		System.out.println(kw[0]);System.out.println(kw[1]);System.out.println(kw[2]);
+		System.out.println();
+	}
+	
 	public static void Kolo() {
-		Scanner read = new Scanner(System.in);
 		double promien;
 		
 		do {
-			System.out.print("Podaj prmien kola [ > ] ");
+			System.out.print("\nPodaj promien [ > ] ");
 			promien = read.nextDouble();
-		} while(promien <= 0); read.close();
+		} while(promien<=0);
 		
 		Kolo kolo1 = new Kolo(promien);
 		//Kolo kolo1 = new Kolo();
@@ -41,8 +50,12 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		Kwadrat();
-		Kolo();
+		//Kwadrat();
+		//Kolo();
+				
+		Kwadrat2();
+		read.close();
+	
 	}
 
 }
